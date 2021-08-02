003da6529baa05220ef02911515b201f996aaad8
Rodrigo Muniz <rodrigo.b.muniz@ibm.com>
2021-08-02 10:29:59 -0300

    begin test on http/matrix

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # manipulate-matrix
 
-##
+## Tests
+
+```
+go test -v ./...
+```
+
+```
+go test -cover ./...
+```
 
 ```
 curl -F 'file=@file/matrix.csv' "localhost:8080/echo"
---
+++ app/delivery/http/matrix_test.go
@@ -0,0 +1,13 @@
+package matrix_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Canary test
+func TestCanarySpec(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(true, true, "Valide if infra for test work")
+}
